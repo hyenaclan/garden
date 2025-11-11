c7ca1b34a7aad7afe8bd5fd0b59bfe8a7b3fdc30
Alexander Trimble <trimble.a@proton.me>
2025-11-10 18:35:09 -0700

    Added cognito deps and poc sign-in and sign-out functionality

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
-import { useApiFetch } from './hooks/useApiFetch';
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+import { useApiFetch } from "./hooks/useApiFetch";
+import { AuthProvider, useAuth } from "react-oidc-context";
+import { cognitoAuthConfig } from "./auth/cognito-config";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,8 +14,29 @@ function App() {
   const buildId = import.meta.env.VITE_BUILD_ID;
   const commitSha = import.meta.env.VITE_COMMIT_SHA?.slice(0, 7);
 
+  const auth = useAuth();
+
   return (
     <>
+      {/* Auth poc */}
+      {auth.isLoading && <p>Loading authentication...</p>}
+      {auth.error && <p>Authentication Error: {auth.error?.message}</p>}
+      {auth.isAuthenticated && (
+        <div>
+          <pre> Hello: {auth.user?.profile.email} </pre>
+          <pre> ID Token: {auth.user?.id_token} </pre>
+          <pre> Access Token: {auth.user?.access_token} </pre>
+          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+          <button onClick={() => auth.removeUser()}>Sign out</button>
+        </div>
+      )}
+      {!auth.isAuthenticated && (
+        <button disabled={auth.isLoading} onClick={() => auth.signinRedirect()}>
+          Sign in
+        </button>
+      )}
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -36,10 +58,12 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p>
-        <button onClick={fetchData} disabled={isLoading}>Fetch from API</button>
-        <h3 className='mt-4 font-bold'>API Output:</h3>
+        <button onClick={fetchData} disabled={isLoading}>
+          Fetch from API
+        </button>
+        <h3 className="mt-4 font-bold">API Output:</h3>
         <p className="bg-white p-2 border border-gray-300 rounded text-sm break-all min-h-[2.5rem]">
-          {apiResponse || 'Click button to fetch data'} 
+          {apiResponse || "Click button to fetch data"}
         </p>
       </p>
       <footer className="text-xs text-gray-500 mt-4">
@@ -49,4 +73,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import { cognitoAuthConfig } from "./auth/cognito-config";
+import { AuthProvider } from "react-oidc-context";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
