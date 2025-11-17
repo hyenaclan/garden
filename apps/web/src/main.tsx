7add096ae3d83e17aff12cb8c3e8769a1886be03
Alexander Trimble <trimble.a@proton.me>
2025-11-16 17:58:08 -0700

    Updated app to use env variables and updated deploy script to set them

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import { getAuthConfig } from "./auth/cognito";
 import { AuthProvider } from "react-oidc-context";
-
-const authConfig = getAuthConfig();
+import { cognitoConfig } from "./auth/cognito";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider {...authConfig}>
+    <AuthProvider {...cognitoConfig}>
       <App />
     </AuthProvider>
   </StrictMode>
