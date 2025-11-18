0f61c6f44be451772136fc38f97ad34a4d5aa082
Alexander Trimble <trimble.a@proton.me>
2025-11-17 17:30:58 -0700

    Added refresh tokens. Addressed PR comments.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -37,15 +37,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>
+      <div>
         <button onClick={fetchData} disabled={isLoading}>
           Fetch from API
         </button>
-        <h3 className="mt-4 font-bold">API Output:</h3>
+        <div className="mt-4 font-bold">API Output:</div>
         <p className="bg-white p-2 border border-gray-300 rounded text-sm break-all min-h-[2.5rem]">
           {apiResponse || "Click button to fetch data"}
         </p>
-      </p>
+      </div>
 
       <footer className="text-xs text-gray-500 mt-4">
         Build #{buildId} ({commitSha})
